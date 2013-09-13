d969034fa985be0f3aa36808caaf93575b386401
zengxiaohanzxh <zengxiaohanzxh@localhost>
2013-09-13 18:32:48 -0500

    added files

---
+++ mysite/blog/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+class Category(models.Model):
+    '''
+    Class for blog categories.
+    '''
+    
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    
+    def __unicode__(self):
+        return self.title
+    def get_absolute_url(self):
+        return reverse('blog-categorized', kwargs={"category_slug": self.slug})
+    
+class Blog(models.Model):
+    '''
+    Class for blog posts.
+    '''
+    
+    title = models.CharField(max_length=300)
+    slug = models.SlugField(unique=True)
+    
+    #author = models.ForeignKey(User, blank=True, null=True)
+    body = models.TextField(help_text="Use textile markup (http://txstyle.org/)")
+    tease = models.TextField(blank=True,
+                             help_text="Use textile markup (http://txstyle.org/)")
+    categories = models.ManyToManyField(Category)
+    image = models.ImageField(upload_to='blog_images', blank=True)
+    allow_comments = models.BooleanField(default=False)
+    highlight = models.BooleanField(default=True,
+                                    help_text='Highlight this post on the front page')
+    body_html = models.TextField(help_text="Valid html (not markup) for the templates", editable=False)
+    tease_html = models.TextField(blank=True, editable=False)
+    
+    def __unicode__(self):
+        return self.title
+
+    def save(self):
+        self.body_html = textile(self.body.encode('utf-8'),
+                                    encoding='utf-8', output='utf-8')
+        self.tease_html = textile(self.tease.encode('utf-8'),
+                                    encoding='utf-8', output='utf-8')
+        super(Post, self).save()
+        
+    def get_absolute_url(self):
+        return reverse('post', kwargs={"slug": self.slug})
---
+++ mysite/mysite/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from blog.models import Blog, Category
+admin.autodiscover()
+admin.site.register(Blog)
+admin.site.register(Category)
+
+urlpatterns = patterns('',
+    # Examples:
+#    url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^mysite/', include('mysite.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
