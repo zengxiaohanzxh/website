6cda78e91a6e2c5b47a38d36906e7b93d1ddd567
Xiaohan Zeng <zengxiaohanzxh@gmail.com>
2014-04-03 22:48:49 -0500

    add data view

--- mysite/mysite/data/models.py
+++ mysite/mysite/data/models.py
@@ -1,24 +1,24 @@
 from django.db import models
 from mysite.common.models import CommonPost, CommonCategory
 
-class Category(CommonCategory):
+class DataCategory(CommonCategory):
     '''
-    Class for blog categories.
+    Class for data categories.
     '''
     
     class Meta:
-        verbose_name_plural = "blog categories"
+        verbose_name_plural = "data categories"
     
     def get_absolute_url(self):
-        return reverse('blog-categorized', kwargs={"category_slug": self.slug})
+        return reverse('data-categorized', kwargs={"category_slug": self.slug})
     
-class Blog(CommonPost):
+class Data(CommonPost):
     '''
-    Class for blog posts.
+    Class for Data posts.
     '''
     
-    categories = models.ManyToManyField(Category)
-    script = models.TextField(help_text="Add scripts here", blank=True)
+    categories = models.ManyToManyField(DataCategory)
+    code = models.TextField(help_text="Add code here", blank=True)
     
     def list_categories(self):
         return ', '.join([category.title for category in self.categories.all()])
--- mysite/mysite/data/views.py
+++ mysite/mysite/data/views.py
@@ -3,35 +3,35 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-from mysite.blog.models import Blog, BlogCategory
+from mysite.data.models import Data, DataCategory
 
-class BlogListView(ListView):
+class DataListView(ListView):
     queryset = Blog.objects.all()
     template_name = "blog/blog_index.html"
     context_object_name = 'list_blogs'
     def get_context_data(self, **kwargs):
         context = super(BlogListView, self).get_context_data(**kwargs)
-        context['list_categories'] = BlogCategory.objects.all()
+        context['list_categories'] = DataCategory.objects.all()
         context['selected_category'] = None
         return context
 
-class CategoryListView(BlogListView):
+class CategoryListView(DataListView):
     def get_context_data(self, **kwargs):
         context = super(CategoryListView, self).get_context_data(**kwargs)
         context['selected_category'] = self.kwargs['category_slug']
         return context
 
     def get_queryset(self):
-        selected_category = get_object_or_404(Category, slug=self.kwargs['category_slug'])
+        selected_category = get_object_or_404(DataCategory, slug=self.kwargs['category_slug'])
         return selected_category.blog_set.all()
 
 
-class BlogDetailView(DetailView):
+class DataDetailView(DetailView):
     model = Blog
     template_name = "blog/blog_content.html"
     context_object_name = 'blog'
     def get_context_data(self, **kwargs):
-        context = super(BlogDetailView, self).get_context_data(**kwargs)
-        context['list_categories'] = BlogCategory.objects.all()
+        context = super(DataDetailView, self).get_context_data(**kwargs)
+        context['list_categories'] = DataCategory.objects.all()
         context['list_blogs'] = Blog.objects.order_by('-pub_date')
         return context
